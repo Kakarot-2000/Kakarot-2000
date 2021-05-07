e93ccb273fb6b4a75edc1cf1c1d2505e8f0df14a
RK <krishna1492000@gmail.com>
2021-05-07 19:06:56 +0530

    added files

---
+++ index.js
@@ -0,0 +1,26 @@
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+
+let DATA = {
+  name: 'Rohan Krishna Ullas',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Asia/Kolkata',
+  }),
+};
+
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+generateReadMe();
